59e2f54e80dd04d896b52c7cf75b73f126b831db
tommyzhang12345678 <102849446+tommyzhang12345678@users.noreply.github.com>
2025-01-06 14:21:05 +0800

    update

--- Python_works3/name_dnd_character_creator.py
+++ Python_works3/name_dnd_character_creator.py
@@ -14,7 +14,3 @@ def a():
     return sum(x) - smallest
 
 print(f"Strength: {a()}, Dexterity: {a()}, Constitution: {a()}, Intelligence: {a()}, Wisdom: {a()}, Charisma: {a()}")
-
-
- #x = [random.randint(1, 6) for _ in range(3)]    
-    #return sum([num for num in x if num != 1])
